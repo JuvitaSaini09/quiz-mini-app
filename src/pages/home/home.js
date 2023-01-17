afcedf1629902573501fd9b7211e6ccad7ffc86e
JuvitaSaini09 <juvitasaini@gmail.com>
2023-01-17 13:24:44 +0530

    refactor : formatted

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,15 @@
 import "./home.css";
 import { quizCategories } from "../../db/categories";
-import {Navbar,Footer} from '../../components/allComponents'
+import { Navbar, Footer } from "../../components/allComponents";
 export const Home = () => {
   return (
     <>
-    <Navbar />
-    <div className="heading-home">
-    <h1 >Welcome to the quiz </h1>
-    <p>Select a category</p>
-    </div>
-      <main className="category-container" style={{border: '10px solid rgba(0,255,0,0.3)'}} >
+      <Navbar />
+      <div className="heading-home">
+        <h1>Welcome to the quiz </h1>
+        <p>Select a category</p>
+      </div>
+      <main className="category-container">
         {quizCategories.map((item) => {
           return (
             <div className="category-card" key={item.id}>
