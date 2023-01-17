c97676e5358072d4b20f63b41bc018958d6aa55c
JuvitaSaini09 <juvitasaini@gmail.com>
2023-01-17 11:03:42 +0530

    fix: added images ,removed browser links

---
+++ src/images/allImages.js
@@ -0,0 +1,10 @@
+import gk  from "../images/gk.webp"
+import culture from "./culture.jpg"
+import dharmic from "./dharmic.jpg"
+import geographic from "./geographic.jpg"
+import history from "./history.jpg"
+import movies from "./movies.jpg"
+import science from "./science.jpg"
+import sports from "./sports.jpg"
+
+export {gk,culture,dharmic,geographic,history,movies,science,sports};
