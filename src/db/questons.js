af719363836e4ef0eb468f8e646d644f2fd8620c
JuvitaSaini09 <juvitasaini@gmail.com>
2023-01-24 00:03:03 +0530

    docs: database updated

--- src/db/questons.js
+++ src/db/questons.js
@@ -27,89 +27,87 @@
     }
   ];
 
-  const culture = [
+  const culture =  [
     {
-      ques: " Which one of the following river flows between Vindhyan and Satpura ranges?",
-      options: ["Narmada", "Mahanadi", "Son", "Netravati"],
-      ans: "Narmada"
+      ques: "The word ‘Sathyameva Jayate’ is taken from __________",
+      options: ["Yajur Veda", "Atharva Veda", "Mundaka Upanishad", "Sam Veda"],
+      ans: "Mundaka Upanishad"   },
+    {
+      ques: "Dandia’ is a popular dance of",
+      options: ["Punjab", "Gujarat", "Tamil Nadu", "Maharashtra"],
+      ans: "Gujarat"
     },
     {
-      ques: "Who among the following wrote Sanskrit grammar?",
-      options: ["Kalidasa", "Charak", "Panini", "Aryabhatt"],
-      ans: "Panini"
-    },
-    {
-      ques: "Which among the following headstreams meets the Ganges in last?",
-      options: ["Alaknanda", "Pindar", "Mandakini", "Bhagirathi"],
-      ans: "Bhagirathi"
+      ques: "Where is Pushkar Fair held?",
+      options: ["Udaipur", "Jaisalmerr", "Jodhpuri", "Ajmer"],
+      ans: "Ajmer"
     },
    {
-      ques: "Which one of the following ports is the oldest port in India?",
-      options: ["Mumbai Port", "Chennai Port", "Kolkata Port", "Vishakhapatnam Port"],
-      ans: "Chennai Port"
+      ques: "Which of the following folk dance forms is associated with Gujarat?",
+      options: ["Nautanki", "Garba", "Kathakali", "Bhangra"],
+      ans: "Garba"
     },
  {
-      ques: "Where was the electricity supply first introduced in India –",
-      options: ["Mumbai", "Dehradun", "Darjeeling", "Chennai"],
-      ans: "Darjeeling"
+      ques: "___________ is classical dance form of Uttar Pradesh",
+      options: ["Kathakali", "Kathak", "Kuchipudi", "Mohiniattam"],
+      ans: "Kathak"
     }
   ];
 
   const dharmic = [
     {
-      ques: " Which one of the following river flows between Vindhyan and Satpura ranges?",
-      options: ["Narmada", "Mahanadi", "Son", "Netravati"],
-      ans: "Narmada"
+      ques: "Who is the king of wealth?",
+      options: ["Agni", "Vayu", "Indra", "Kubera"],
+      ans: "Kubera"   },
+    {
+      ques: "Where is Tirupati Balaji (Tirumala Venkateswara Temple) present?",
+      options: ["Chittor, Andhra Pradesh", "Vishakapatnam, Andhra Pradesh", "Kolkata, West Bengal", "Cuttack, Odisha"],
+      ans: "Chittor, Andhra Pradesh"
     },
     {
-      ques: "Who among the following wrote Sanskrit grammar?",
-      options: ["Kalidasa", "Charak", "Panini", "Aryabhatt"],
-      ans: "Panini"
-    },
-    {
-      ques: "Which among the following headstreams meets the Ganges in last?",
-      options: ["Alaknanda", "Pindar", "Mandakini", "Bhagirathi"],
-      ans: "Bhagirathi"
+      ques: "Who is the wife of ‘Ravana’?",
+      options: ["Shrutakirti", "Simhikar", "Mandavii", "Mandodari"],
+      ans: "Mandodari"
     },
    {
-      ques: "Which one of the following ports is the oldest port in India?",
-      options: ["Mumbai Port", "Chennai Port", "Kolkata Port", "Vishakhapatnam Port"],
-      ans: "Chennai Port"
+      ques: "The ‘Rath Yatra at Puri’ is celebrated in honour of which Hindu deity?",
+      options: ["Ram", "Jaagannath", "Shiva", "Vishnu"],
+      ans: "Jaagannath"
     },
  {
-      ques: "Where was the electricity supply first introduced in India –",
-      options: ["Mumbai", "Dehradun", "Darjeeling", "Chennai"],
-      ans: "Darjeeling"
+      ques: "The number of Puranas is",
+      options: ["18", "21", "23", "16"],
+      ans: "18"
     }
   ];
 
   const movies= [
     {
-      ques: " Which one of the following river flows between Vindhyan and Satpura ranges?",
-      options: ["Narmada", "Mahanadi", "Son", "Netravati"],
-      ans: "Narmada"
+      ques: "In film “zindagi na milengi dobara” , where did Three Friends Meet ?",
+      options: ["America", "Mumbai", "Spain", "Italy"],
+      ans: "Spain"   },
+    {
+      ques: "Actor Sushant Singh Rajput was born in _ city ?",
+      options: ["Ranchi", "Bhopal", "Kolkata", "Patna"],
+      ans : "Patna"
     },
     {
-      ques: "Who among the following wrote Sanskrit grammar?",
-      options: ["Kalidasa", "Charak", "Panini", "Aryabhatt"],
-      ans: "Panini"
-    },
-    {
-      ques: "Which among the following headstreams meets the Ganges in last?",
-      options: ["Alaknanda", "Pindar", "Mandakini", "Bhagirathi"],
-      ans: "Bhagirathi"
+      ques: "Actor Vidyut Jammwal is Famous for his _ ?",
+      options: ["height", "Romantic role", "Social media interaction", "Live Stunt"],
+      ans: "Live Stunt"
     },
    {
-      ques: "Which one of the following ports is the oldest port in India?",
-      options: ["Mumbai Port", "Chennai Port", "Kolkata Port", "Vishakhapatnam Port"],
-      ans: "Chennai Port"
+      ques: "Which Following Actress debuted opposite Shahrukh Khan in movie “Om shanti Om”?",
+      options: ["Sonam Kapoor", "Anushka Sharma", "Deepika Padukone", "Priyanka Chopra"],
+      ans: "Deepika Padukone"
     },
  {
-      ques: "Where was the electricity supply first introduced in India –",
-      options: ["Mumbai", "Dehradun", "Darjeeling", "Chennai"],
-      ans: "Darjeeling"
+      ques: "Which of the following natural calamity Shown in the Film ‘ Kedarnath’ ?",
+      options: ["Earthquake", "Cyclone", "Flood", "Landsliding"],
+      ans: "Flood"
     }
   ];
+
 
   const history = [
     {
