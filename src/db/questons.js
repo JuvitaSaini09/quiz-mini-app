df4b1f9715eb22e86cb3f8cdce5658fc86cefe52
JuvitaSaini09 <juvitasaini@gmail.com>
2023-01-25 14:57:51 +0530

    docs: database updated

--- src/db/questons.js
+++ src/db/questons.js
@@ -111,119 +111,118 @@
 
   const history = [
     {
-      ques: " Which one of the following river flows between Vindhyan and Satpura ranges?",
-      options: ["Narmada", "Mahanadi", "Son", "Netravati"],
-      ans: "Narmada"
+      ques: "Which the following rulers is known as the ‘Napoleon of India’?",
+      options: ["Samudragupta", "Ashoka", "Chandragupta I", "Skandgupta"],
+      ans: "Samudragupta"   },
+    {
+      ques: "First battle of Panipat was fought between?",
+      options: ["Babur and Lodi", "Akbar and Hemu","Mughal and British", "Akbar and Lodi"],
+      ans: "Babur and Lodi"
     },
     {
-      ques: "Who among the following wrote Sanskrit grammar?",
-      options: ["Kalidasa", "Charak", "Panini", "Aryabhatt"],
-      ans: "Panini"
-    },
-    {
-      ques: "Which among the following headstreams meets the Ganges in last?",
-      options: ["Alaknanda", "Pindar", "Mandakini", "Bhagirathi"],
-      ans: "Bhagirathi"
+      ques: "Azad Hind was created with the assistance of",
+      options: ["USA", "Russia", "Japan", "China"],
+      ans: "Japan"
     },
    {
-      ques: "Which one of the following ports is the oldest port in India?",
-      options: ["Mumbai Port", "Chennai Port", "Kolkata Port", "Vishakhapatnam Port"],
-      ans: "Chennai Port"
+      ques: "The Battle of Plassey was fought in”?",
+      options: ["1757","1782","1748","1764"],
+      ans: "1757"
     },
  {
-      ques: "Where was the electricity supply first introduced in India –",
-      options: ["Mumbai", "Dehradun", "Darjeeling", "Chennai"],
-      ans: "Darjeeling"
+      ques: "Between which of the following was the ancient town of Takshashila located?",
+      options: ["Ravi and Beas", "Jhelum and Chenab", "Indus and Jhelum", "Chenab and Ravi"],
+      ans: "Indus and Jhelum"
     }
   ];
-
 
   const science = [
     {
-      ques: " Which one of the following river flows between Vindhyan and Satpura ranges?",
-      options: ["Narmada", "Mahanadi", "Son", "Netravati"],
-      ans: "Narmada"
+      ques: "Which among the following sugars is maximum in Honey?",
+      options: ["Glucose", "Sucrose", "Fructose", "Maltose"],
+      ans: "Fructose"   },
+    {
+      ques: "‘Ophiology’ is the study of which of the following animals?",
+      options: ["Moths and Butterflies", "Snakes","Whales", "Ants"],
+      ans: "Snakes"
     },
     {
-      ques: "Who among the following wrote Sanskrit grammar?",
-      options: ["Kalidasa", "Charak", "Panini", "Aryabhatt"],
-      ans: "Panini"
-    },
-    {
-      ques: "Which among the following headstreams meets the Ganges in last?",
-      options: ["Alaknanda", "Pindar", "Mandakini", "Bhagirathi"],
-      ans: "Bhagirathi"
+      ques: "Which of the following waves are also called as heat waves?",
+      options: ["UV rays", "X-rays", "Infrared", "Micro waves"],
+      ans: "Infrared"
     },
    {
-      ques: "Which one of the following ports is the oldest port in India?",
-      options: ["Mumbai Port", "Chennai Port", "Kolkata Port", "Vishakhapatnam Port"],
-      ans: "Chennai Port"
+      ques: "Cinnamon is obtained from which part of the plant?",
+      options: ["Fruits","Stem","Bark","Roots"],
+      ans: "Bark"
     },
  {
-      ques: "Where was the electricity supply first introduced in India –",
-      options: ["Mumbai", "Dehradun", "Darjeeling", "Chennai"],
-      ans: "Darjeeling"
+      ques: "Which gas is taken in by plants in the process of photosynthesis ?",
+      options: ["Oxygen", "Carbon dioxide", "Nitrogen", "Argon"],
+      ans: "Carbon dioxide"
     }
   ];
-
+  
 
   const sports = [
     {
-      ques: " Which one of the following river flows between Vindhyan and Satpura ranges?",
-      options: ["Narmada", "Mahanadi", "Son", "Netravati"],
-      ans: "Narmada"
+      ques: "Who among the following was known as Flying Sikh ?",
+      options: ["Milkha Singh", "Harbhajan Singh", "Yuvaraj Singh", "Gurbachan Singh"],
+      ans: "Milkha Singh"   },
+    {
+      ques: "Who is the first Indian woman wrestler to win a medal in the Olympics?",
+      options: ["Sakshi Mailk", "Mary Kom","Geeta Phogat", "Saikom Mirabai Chanu"],
+      ans: "Sakshi Mailk"
     },
     {
-      ques: "Who among the following wrote Sanskrit grammar?",
-      options: ["Kalidasa", "Charak", "Panini", "Aryabhatt"],
-      ans: "Panini"
-    },
-    {
-      ques: "Which among the following headstreams meets the Ganges in last?",
-      options: ["Alaknanda", "Pindar", "Mandakini", "Bhagirathi"],
-      ans: "Bhagirathi"
+      ques: "Where were the first Asian Games held ?",
+      options: ["Mumbai", "Bhopal", "New Delhi", "Chennai"],
+      ans: "New Delhi"
     },
    {
-      ques: "Which one of the following ports is the oldest port in India?",
-      options: ["Mumbai Port", "Chennai Port", "Kolkata Port", "Vishakhapatnam Port"],
-      ans: "Chennai Port"
+      ques: " Neeraj Chopra win the first ever olympic gold medal for India in which of the following sports ?",
+      options: ["400 m hurdles","Javelin Throw","High Jump","Long Jump"],
+      ans: "Javelin Throw"
     },
  {
-      ques: "Where was the electricity supply first introduced in India –",
-      options: ["Mumbai", "Dehradun", "Darjeeling", "Chennai"],
-      ans: "Darjeeling"
+      ques: "Who became the first player of Indian origin to play in an NBA game ?",
+      options: ["Sim Bhullar", "Palpreet Singh", "Amjyot Singh", "Satnam Singh"],
+      ans: "Sim Bhullar"
     }
   ];
-  
+
+
+
 
   const geographic = [
     {
-      ques: " Which one of the following river flows between Vindhyan and Satpura ranges?",
-      options: ["Narmada", "Mahanadi", "Son", "Netravati"],
-      ans: "Narmada"
+      ques: "Which among the following states has highest population density ?",
+      options: ["Maharastra", "Bihar", "West Bengal", "Uttar Pradesh"],
+      ans: "Bihar"   },
+    {
+      ques: "Sagar Matha is another name of which of the following mountain Peak ?",
+      options: ["Nanda Devi", "Kamet","Mt. Everest", "Annapurna"],
+      ans: "Mt. Everest"
     },
     {
-      ques: "Who among the following wrote Sanskrit grammar?",
-      options: ["Kalidasa", "Charak", "Panini", "Aryabhatt"],
-      ans: "Panini"
-    },
-    {
-      ques: "Which among the following headstreams meets the Ganges in last?",
-      options: ["Alaknanda", "Pindar", "Mandakini", "Bhagirathi"],
-      ans: "Bhagirathi"
+      ques: "Which of the following is an important river of the Indian desert ?",
+      options: ["Narmada", "Jhelum", "Tapi", "Luni"],
+      ans: "Luni"
     },
    {
-      ques: "Which one of the following ports is the oldest port in India?",
-      options: ["Mumbai Port", "Chennai Port", "Kolkata Port", "Vishakhapatnam Port"],
-      ans: "Chennai Port"
+      ques: "Which of the following hill stations is known as the “Mini Switzerland of India” ?",
+      options: ["Chail","Shimla","Manali","Khajjar"],
+      ans: "Khajjar"
     },
  {
-      ques: "Where was the electricity supply first introduced in India –",
-      options: ["Mumbai", "Dehradun", "Darjeeling", "Chennai"],
-      ans: "Darjeeling"
+      ques: "The place located at the confluence of Alaknanda and Bhagirathi rivers is known as which of the following ?",
+      options: ["Badrinath", "Rishikesh", "Devprayag", "Rudraprayag"],
+      ans: "Devprayag"
     }
   ];
-  
+
+
+
 
   export const allCategoryQuestions = [generalKnowledge,culture,movies,history,dharmic,science,sports,geographic];
   
