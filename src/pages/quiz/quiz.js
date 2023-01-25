97eae4a74032bd42c617e5ab41cbf078854f718d
JuvitaSaini09 <juvitasaini@gmail.com>
2023-01-25 18:55:37 +0530

    docs : added confirm button

--- src/pages/quiz/quiz.js
+++ src/pages/quiz/quiz.js
@@ -1,17 +1,19 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { NavLink, useParams, useNavigate } from "react-router-dom";
-import { Modal, Navbar} from "../../components/allComponents";
+import { Modal, Navbar } from "../../components/allComponents";
 import { useRuleModalContext } from "../../context/ruleModal/ruleModalContext";
-import {allCategoryQuestions} from "../../db/questons" 
+import { allCategoryQuestions } from "../../db/questons";
 import "./quiz.css";
-import {badge,trophy} from "../../images/allImages"
+import { badge, trophy } from "../../images/allImages";
 
 export const Quiz = () => {
-  const {isRulesDisplayed }= useRuleModalContext();
+  const { isRulesDisplayed } = useRuleModalContext();
   const { category } = useParams();
   const [quesNumber, setQuestionNumber] = useState(0);
   const [score, setScore] = useState(0);
   const [userAnswer, setUserAnswer] = useState(null);
+  const [confirmEvent, setConfirmEvent] = useState(null);
+  const [confirmedClicked, setConfirmedClicked] = useState(false);
 
   const quizData = allCategoryQuestions[category];
   const [result, setResult] = useState(false);
@@ -24,83 +26,133 @@ export const Quiz = () => {
       setQuestionNumber((old) => old + 1);
     }
 
-    setUserAnswer(null)
+    setUserAnswer(null);
+    // confirmEvent.target.classList.remove('bg-green');
+    // confirmEvent.target.classList.remove('bg-red');
+    setConfirmedClicked(false);
+    setConfirmEvent(null);
   };
-  
-  const handleAnswerResponse = (e) => {
-    setUserAnswer(e);
+
+  const handleConfirm = () => {
+    setConfirmedClicked(true);
+    if (userAnswer === quizData[quesNumber].ans) {
+      confirmEvent.target.classList.add("bg-green");
+    } else {
+      confirmEvent.target.classList.add("bg-red");
+    }
   };
+
+  const handleAnswerResponse = (element, event) => {
+    setUserAnswer(element);
+    setConfirmEvent(event);
+  };
+
   return (
     <>
-    <Navbar />
-    <div className={
-          isRulesDisplayed ? "quiz-body block-background" : "quiz-body"
-        }>
+      <Navbar />
       <div
-        className="quiz-question"
+        className={
+          isRulesDisplayed ? "quiz-body block-background" : "quiz-body"
+        }
       >
-        
-         {/*<------- quiz questions ---->  */}
+        <div className="quiz-question">
+          {/*<------- quiz questions ---->  */}
 
-         {result ? (
-        <div>
-          <div className="result-title"><h1>Scores</h1> <img src={badge} /></div>
-          <div className="result-scores-heading">your scores are  </div> 
-         <div className="result-scores">
-         <p>{score}/50 </p> <img className="trophy" src={trophy} /> 
-         </div>
-          <button className="start-again-btn">Start Again</button>
-        </div>
-      ) : (
-        <div className="ques-body">
-          {" "}
-          <h3 className="quiz-heading">
-          Dash category Quiz
-          </h3>
-        
-          <div className="ques-container">
-            <div className="ques">
-              <span className="ques-number">Ques {quesNumber + 1} : {quizData[quesNumber].ques}</span> 
+          {result ? (
+            <div>
+              <div className="result-title">
+                <h1>Scores</h1> <img src={badge} />
+              </div>
+              <div className="result-scores-heading">your scores are </div>
+              <div className="result-scores">
+                <p>{score}/50 </p> <img className="trophy" src={trophy} />
+              </div>
+              <button className="start-again-btn">Start Again</button>
             </div>
+          ) : (
+            <div className="ques-body">
+              {" "}
+              <h3 className="quiz-heading">Dash category Quiz</h3>
+              <div className="ques-container">
+                <div className="ques">
+                  <span className="ques-number">
+                    Ques {quesNumber + 1} : {quizData[quesNumber].ques}
+                  </span>
+                </div>
 
-            <div className="option-btns-container">
-              {quizData[quesNumber].options.map((element) => (
-                <button key={element} className="option-btns" onClick={() => handleAnswerResponse(element)}>
-                  {element}
+                <div
+                  className="option-btns-container"
+                  style={
+                    confirmedClicked
+                      ? { pointerEvents: "none" }
+                      : { pointerEvents: "auto" }
+                  }
+                >
+                  {quizData[quesNumber].options.map((element) => (
+                    <div
+                      key={element}
+                      className={
+                        confirmedClicked
+                          ? quizData[quesNumber].ans === element
+                            ? "bg-green-button"
+                            : ""
+                          : ""
+                      }
+                    >
+                      <button
+                        className={
+                          userAnswer === null || userAnswer != element
+                            ? "option-btns"
+                            : "option-btns selected-option"
+                        }
+                        onClick={(event) =>
+                          handleAnswerResponse(element, event)
+                        }
+                      >
+                        {element}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+                <button
+                  className="confirm-btn"
+                  style={
+                    userAnswer === null
+                      ? { pointerEvents: "none", opacity: 0.5 }
+                      : { pointerEvents: "auto", opacity: 1 }
+                  }
+                  onClick={handleConfirm}
+                >
+                  Confirm
                 </button>
-              ))}
+                <button
+                  className="next-btn"
+                  onClick={incrementQuestionNUmber}
+                  style={
+                    confirmedClicked
+                      ? { pointerEvents: "auto", opacity: 1 }
+                      : { pointerEvents: "none", opacity: 0.5 }
+                  }
+                >
+                  {" "}
+                  Next
+                </button>
+              </div>
             </div>
+          )}
 
-            <button className="next-btn"
-              onClick={incrementQuestionNUmber}
-              style={
-                userAnswer === null
-                  ? { pointerEvents: "none", opacity: 0.5 }
-                  : { pointerEvents: "auto", opacity: 1 }
-              }
-            > Next</button>
+          <div>
+            <div></div>
           </div>
         </div>
-      )}
-
-
-
-         <div>
-           <div></div>
-           
-         </div>
       </div>
-
-      
-    </div>
-{/*<------- rule modal ---->  */}
-<div
+      {/*<------- rule modal ---->  */}
+      <div
         className="modal-container"
         style={isRulesDisplayed ? { display: "block" } : { display: "none" }}
       >
         <Modal />
       </div>
-
     </>
   );
 };
